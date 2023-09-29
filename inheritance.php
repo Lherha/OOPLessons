1aebd0e4dfb7522bb4f7bea2fbf973451f3da915
Ajao Mueez <ajaomueez007@gmail.com>
2023-09-29 17:58:29 +0100

    modified:   inheritance.php

--- inheritance.php
+++ inheritance.php
@@ -132,7 +132,7 @@ class Employee extends Person
     /**
      * Set the salary of the employee.
      *
-     * @param int $newSalary The new salary of the employee.
+     * @param int $newSalary
      */
     public function setSalary($newSalary)
     {
