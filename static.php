a72177ff4c6f2a473e16745e7b1e4848dccd00ef
Ajao Mueez <ajaomueez007@gmail.com>
2023-10-02 16:15:12 +0100

    modified:   static.php

--- static.php
+++ static.php
@@ -1,51 +1,87 @@
 <?php
 
 /**
- * Add a static method in the Person class
- * to count the number of Person Objects created
+ * Class Person
+ *
+ * Represents a person with a name and age. Provides a static method to count the number of Person objects created.
  */
-
 class Person
 {
-  public $name;
-  private $age;
+    /**
+     * @var string The name of the person.
+     */
+    public $name;
 
-  private static $count = 0;
+    /**
+     * @var int The age of the person.
+     */
+    private $age;
 
-  public function __construct($name, $age)
-  {
-    $this->name = $name;
-    $this->age = $age;
+    /**
+     * @var int Keeps track of the number of Person objects created.
+     */
+    private static $count = 0;
 
-    // When Person class is Instanciated, increment the count variable
-    self::$count++;
-  }
+    /**
+     * Person constructor.
+     *
+     * @param string $name The name of the person.
+     * @param int $age The age of the person.
+     */
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
 
-  public function getName()
-  {
-    return $this->name;
-  }
+        // When Person class is instantiated, increment the count variable
+        self::$count++;
+    }
 
-  public function getAge()
-  {
-    return $this->age;
-  }
+    /**
+     * Get the name of the person.
+     *
+     * @return string The name of the person.
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
 
-  public function setAge($newAge)
-  {
-    $this->age = $newAge;
-  }
+    /**
+     * Get the age of the person.
+     *
+     * @return int The age of the person.
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
 
-  public static function countCreatedObjects()
-  {
-    return self::$count;
-  }
+    /**
+     * Set the age of the person.
+     *
+     * @param int $newAge The new age of the person.
+     */
+    public function setAge($newAge)
+    {
+        $this->age = $newAge;
+    }
+
+    /**
+     * Get the count of Person objects created.
+     *
+     * @return int The count of Person objects created.
+     */
+    public static function countCreatedObjects()
+    {
+        return self::$count;
+    }
 }
 
 $person1 = new Person('Person One', 20);
-$person2 = new Person('Person One', 25);
-$person3 = new Person('Person One', 30);
-$person4 = new Person('Person One', 50);
-$person5 = new Person('Person One', 27);
+$person2 = new Person('Person Two', 25);
+$person3 = new Person('Person Three', 30);
+$person4 = new Person('Person Four', 50);
+$person5 = new Person('Person Five', 27);
 
-echo 'The Person Object was instanciated ' . Person::countCreatedObjects() . ' times';
+echo 'The Person object was instantiated ' . Person::countCreatedObjects() . ' times';
