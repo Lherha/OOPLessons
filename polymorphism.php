bdf98b1a512154fa0e733574562256a7a9cd8c54
Ajao Mueez <ajaomueez007@gmail.com>
2023-10-02 16:24:35 +0100

    modified:   polymorphism.php

--- polymorphism.php
+++ polymorphism.php
@@ -1,21 +1,44 @@
 <?php
+/**
+ * Class Message
+ *
+ * Represents a message formatter.
+ */
 class Message
 {
-  public function formatMessage($message)
-  {
-    return printf("<i>%s</i>", $message . '<br>');
-  }
+    /**
+     * Format the message to be displayed in italics.
+     *
+     * @param string $message The input message to be formatted.
+     * @return int The length of the formatted message.
+     */
+    public function formatMessage($message)
+    {
+        return printf("<i>%s</i>", $message . '<br>');
+    }
 }
+
+/**
+ * Class BoldMessage
+ *
+ * Represents a message formatter that displays messages in bold.
+ */
 class BoldMessage extends Message
 {
-  public function formatMessage($message)
-  {
-    return printf("<b>%s</b>", $message);
-  }
+    /**
+     * Format the message to be displayed in bold.
+     *
+     * @param string $message The input message to be formatted.
+     * @return int The length of the formatted message.
+     */
+    public function formatMessage($message)
+    {
+        return printf("<b>%s</b>", $message);
+    }
 }
+
 $message = new Message();
 $message->formatMessage('Hello World');
 
 $message = new BoldMessage();
 $message->formatMessage('Welcome Here!');  
-?>
