13ec9b8a2b8be780cb147569a15f1adca6125c91
Ajao Mueez <ajaomueez007@gmail.com>
2023-10-04 09:59:16 +0100

    modified:   crud/class.php

--- crud/class.php
+++ crud/class.php
@@ -1,10 +1,20 @@
 <?php
+
+/**
+ * Class Person
+ *
+ * Represents a basic person with properties like name and age,
+ * and methods to retrieve and set the name and age.
+ * @author Ajao Mueez <mueezlherha@gmail.com>
+ * @copyright 2010 Ennui Design 
+ * @license http://www.php.net/license/3_01.txt PHP License 3.01
+ */
 class Person
 {
     /** 
   * A public variable 
   * 
-  * @var string stores data for the class 
+  * @var string
   */
   public $name;
   public $age;
