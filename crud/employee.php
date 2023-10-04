06a545cb5f6ce536129ca6536dbf24ce75a610af
Ajao Mueez <ajaomueez007@gmail.com>
2023-10-04 10:12:34 +0100

    modified:   crud/employee.php

--- crud/employee.php
+++ crud/employee.php
@@ -1,11 +1,31 @@
 <?php
+
+/**
+ * Class Employee
+ * Represents an employee extending the Person class.
+ */
 include 'class.php';
 
 class Employee extends Person
 {
+    /**
+     * @var string The position of the employee.
+     */
     private $position;
+
+    /**
+     * @var float The salary of the employee.
+     */
     private $salary;
 
+    /**
+     * Employee constructor.
+     *
+     * @param string $employee_name The name of the employee.
+     * @param int $employee_age The age of the employee.
+     * @param string $position The position of the employee.
+     * @param float $salary The salary of the employee.
+     */
     public function __construct($employee_name, $employee_age, $position, $salary)
     {
         parent::__construct($employee_name, $employee_age);
@@ -13,26 +33,51 @@ class Employee extends Person
         $this->salary = $salary;
     }
 
+    /**
+     * Get the position of the employee.
+     *
+     * @return string The position of the employee.
+     */
     public function getPosition()
     {
         return $this->position;
     }
 
+    /**
+     * Set the position of the employee.
+     *
+     * @param string $newPosition The new position to set.
+     */
     public function setPosition($newPosition)
     {
         $this->position = $newPosition;
     }
 
+    /**
+     * Get the salary of the employee.
+     *
+     * @return float The salary of the employee.
+     */
     public function getSalary()
     {
         return $this->salary;
     }
 
+    /**
+     * Set the salary of the employee.
+     *
+     * @param float $newSalary The new salary to set.
+     */
     public function setSalary($newSalary)
     {
         $this->salary = $newSalary;
     }
 
+    /**
+     * Add employee data to the database.
+     *
+     * @param PDO $con The database connection.
+     */
     public function addToDatabase($con)
     {
         $fullname = $this->getName();
@@ -56,4 +101,3 @@ class Employee extends Person
         }
     }
 }
-?>
